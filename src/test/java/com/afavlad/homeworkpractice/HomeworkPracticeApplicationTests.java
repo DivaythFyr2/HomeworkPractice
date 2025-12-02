039be55d93c659da7d84b022e78e38960e86ee28
Vlad Afanasev <vlad-vladik-afanasev@mail.ru>
2025-12-02 15:42:01 +0300

    Initial commit

---
+++ src/test/java/com/afavlad/homeworkpractice/HomeworkPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afavlad.homeworkpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
